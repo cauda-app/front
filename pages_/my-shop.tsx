275d32c32b00a4f14b10fb92c2129e0164bc4ffc
Mauricio Grijalba <grijos@gmail.com>
2020-04-11 15:09:38 -0300

    Design: Edit Shop

--- pages_/my-shop.tsx
+++ pages_/my-shop.tsx
@@ -4,8 +4,6 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import Link from 'next/link';
 
@@ -35,7 +33,7 @@ const MyShop = () => {
             </Form.Group>
 
             <Button
-              href=""
+              href="/edit-shop"
               variant="primary"
               size="lg"
               className="mb-3"
@@ -46,79 +44,6 @@ const MyShop = () => {
               {t('common:accept-by-continue')} <Link href="/terms"><a>{t('common:terms-conditions')}</a></Link>. 
             </small>
 
-          </Form>
-
-        </Card.Body>
-      </Card>
-
-
-      <Card className="cauda_card mb-4 mx-auto">
-        <Card.Header className="text-center">
-          {t('common:shop-details')}
-        </Card.Header>
-        {/* <div className="shop_map">
-          <Card.Img variant="top" src="map.png" alt="ShopMap" />
-        </div> */}
-        <Card.Body>
-
-          <Form>
-
-            <Form.Group controlId="shop-name">
-              <Form.Label className="sr-only">{t('common:shop-name')}</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>Icon</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder={t('common:shop-name')}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="shop-address">
-              <Form.Label className="sr-only">{t('common:shop-address')}</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>Icon</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder={t('common:shop-address')}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="shop-phone">
-              <Form.Label className="sr-only">{t('common:shop-phone')}</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>Icon</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder={t('common:shop-phone')}
-                />
-              </InputGroup>
-            </Form.Group>
-
-            <Form.Group controlId="shop-hours">
-              <Form.Label className="sr-only">{t('common:shop-hours')}</Form.Label>
-              <InputGroup>
-                <InputGroup.Prepend>
-                  <InputGroup.Text>Icon</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder={t('common:shop-hours')}
-                />
-              </InputGroup>
-            </Form.Group>
- 
-            <Button
-              href=""
-              variant="success"
-              size="lg"
-              className="mt-4"
-              block
-            >{t('common:save')}</Button>
-          
           </Form>
 
         </Card.Body>
