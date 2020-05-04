bf924943dc2a7160d6f3229b5fe584d62b13bad9
Ramiro Vazquez <ramirovazquez@gmail.com>
2020-05-03 21:40:10 -0300

    add require login helper

--- pages_/[shopId].tsx
+++ pages_/[shopId].tsx
@@ -1,12 +1,12 @@
 import { GetServerSideProps } from 'next';
 
 import prismaClient from '../prisma/client';
-import { processCookie } from './api/graphql';
 import ShopCard from 'src/components/ShopCard';
-import Layout from '../src/components/Layout';
+import Layout from 'src/components/Layout';
 import { isOpen, shopPhone, status } from '../graphql/shop/helpers';
+import { requireLogin } from 'src/utils/next';
 
-const RequestTurn = ({ shop, ...rest }) => {
+const RequestTurn = ({ shop }) => {
   return (
     <Layout>
       <ShopCard shop={shop} />
@@ -15,16 +15,13 @@ const RequestTurn = ({ shop, ...rest }) => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const token = processCookie(context.req);
   const shopId = context.params?.shopId as string | undefined;
-  let shop;
-
-  if (!token || !token.isValid) {
-    context.res.writeHead(303, {
-      Location: '/register-phone?redirectTo=' + `/${shopId}`,
-    });
-    context.res.end();
+  const token = requireLogin(context);
+  if (!token) {
+    return { props: {} };
   }
+
+  let shop;
 
   if (shopId) {
     const dbShop = await prismaClient.shopDetails.findOne({
