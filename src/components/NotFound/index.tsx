5cac9b27e2bebfd3c032d436213dc960b6f274f4
grijos <grijos@gmail.com>
2020-05-18 14:09:17 -0300

    Design: Add 404 & 500 pages #44

--- src/components/NotFound/index.tsx
+++ src/components/NotFound/index.tsx
@@ -1,12 +1,34 @@
 import useTranslation from 'next-translate/useTranslation';
 import Layout from '../Layout';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 export default function NotFound() {
   const { t } = useTranslation();
 
   return (
     <Layout>
-      <h1>404 - {t('common:not-found')}</h1>
+      <Row>
+        <Col xs="auto" className="mx-auto">
+          <div className="text-center">
+            <h1 className="display-1 text-secondary">404</h1>
+            <p className="alert alert-secondary">{t('common:not-found')}</p>
+            <Button
+              variant="primary"
+              href="/"
+              block
+              className="d-flex justify-content-between align-items-center py-2"
+            >
+              <FontAwesomeIcon icon={faArrowLeft} fixedWidth />
+              {t('common:go-home')}
+              <div></div>
+            </Button>
+          </div>
+        </Col>
+      </Row>
     </Layout>
   );
 }
