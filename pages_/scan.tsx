cc473596a9c06783aa6bb493a4e28a4b1a7cd5cc
Ramiro Vazquez <ramirovazquez@gmail.com>
2020-05-20 21:24:50 -0300

    go to shop on valid code

--- pages_/scan.tsx
+++ pages_/scan.tsx
@@ -1,15 +1,14 @@
 import React, { ComponentType } from 'react';
 import dynamic from 'next/dynamic';
+import Router from 'next/router';
 import useTranslation from 'next-translate/useTranslation';
 import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import Button from 'react-bootstrap/Button';
 import Spinner from '../src/components/Spinner';
 import Layout from '../src/components/Layout';
 import { ReaderProps } from '../src/types/react-qr-reader';
-import img_templateUrl from '../public/cauda_qrcode@3x.png';
 import img_scanningUrl from '../public/cauda_scanqr@3x.png';
-import GoBack from 'src/components/GoBack';
-import Link from 'next/link';
-import Button from 'react-bootstrap/Button';
 
 const QrReader: ComponentType<ReaderProps> = dynamic(
   () => import('react-qr-reader'),
@@ -18,91 +17,66 @@ const QrReader: ComponentType<ReaderProps> = dynamic(
   }
 );
 
-const QrTemplate = () => (
-  <div>
-    <img className="img-fluid" src={img_templateUrl}></img>
-  </div>
-);
-const Loading = () => {
-  const { t } = useTranslation();
-
-  return (
-    <div className="mt-1 d-flex align-content-center justify-content-center">
-      <Spinner />
-      <p className="ml-2 mt-1 mb-0">{t('common:loading')}</p>
-    </div>
-  );
-};
-
 const Scan = () => {
   const { t } = useTranslation();
-  const [error, setError] = React.useState(false);
-  const [processing, setProcessing] = React.useState(false);
+  const [codeError, setCodeError] = React.useState(false);
+  const [readerError, setReaderError] = React.useState(false);
   const [loaded, setLoaded] = React.useState(false);
-  const [result, setResult] = React.useState(false);
 
-  const handleError = (error) => setError(error);
+  const handleError = () => setReaderError(true);
   const handleLoad = () => setLoaded(true);
   const handleScan = (result) => {
     if (result) {
-      setProcessing(true);
-      setResult(result);
+      try {
+        const url = new URL(result);
+        if (url.origin === window.location.origin) {
+          Router.push(url.pathname);
+        } else {
+          setCodeError(true);
+        }
+      } catch (error) {
+        setCodeError(true);
+      }
     }
   };
 
   return (
     <Layout>
       <div className="content d-flex flex-column justify-content-between">
-        <Card className="cauda_card cauda_scan p-4 text-center">
+        <Card className="cauda_card cauda_scan p-4 text-center mb-3">
           <Card.Body className="p-0">
-            {error ? (
+            {readerError ? (
               t('common:qr-reader-error')
             ) : (
               <>
                 <div className="scanning_bar">
                   <img className="img-fluid" src={img_scanningUrl}></img>
                 </div>
-
-                {processing ? (
-                  <QrTemplate />
-                ) : (
-                  <QrReader
-                    delay={300}
-                    onError={handleError}
-                    onScan={handleScan}
-                    onLoad={handleLoad}
-                    showViewFinder={false}
-                  />
-                )}
-                {loaded ? null : <Loading />}
-
-                {processing ? (
-                  <>
-                    <p className="mb-0">{result}</p>
-                  </>
-                ) : null}
+                <QrReader
+                  delay={300}
+                  onError={handleError}
+                  onScan={handleScan}
+                  onLoad={handleLoad}
+                  showViewFinder={false}
+                />
+                {loaded ? null : <Spinner />}
               </>
             )}
           </Card.Body>
         </Card>
-      </div>
-      <div className="p-4">
-        <Link href={'/'} passHref>
-          <Button
-            block
-            type="button"
-            variant="danger"
-            size="lg"
-            className="mt-4"
-          >
+
+        {codeError ? (
+          <p className="alert alert-secondary">
+            {t('common:invalid-code-error')}
+          </p>
+        ) : null}
+
+        <Link href="/" passHref>
+          <Button block type="button" variant="danger" size="lg">
             {t('common:cancel')}
           </Button>
         </Link>
       </div>
-      <style jsx>{`
-        .cauda_scan {
-        }
-      `}</style>
     </Layout>
   );
 };
